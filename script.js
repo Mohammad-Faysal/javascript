b3e9239ccaf232166ca5e7a7cae14b752711fc5a
unknown <faisalcox2041gmail.com>
2022-08-14 16:42:09 +0600

    first javascript class

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("hlw, world");
+console.log("hi safa!");
+console.log("hi safa!");
+console.log(33 + 55);
+let newPen = 5;
+let newBook = 10;
+console.log(newPen + newBook);
+/* hhh
+kkkk
+*/
+let isThisCat = false;
+console.log(typeof isThisCat);
+const oldAge = 20;
+const currentAge = 25;
+console.log(oldAge - currentAge);
+console.log(
+  ` I am
+  a 
+  programmer`
+);
+
+const birthYear = 2002;
+if (birthYear <= 2000) {
+  console.log("You are to old 🤦‍♂️");
+} else {
+  console.log("You are not young!");
+}
+
+const marks = 34;
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("A");
+} else if (marks >= 60) {
+  console.log("A-");
+} else if (marks >= 50) {
+  console.log("B+");
+} else if (marks >= 40) {
+  console.log("C");
+} else if (marks >= 33) {
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
